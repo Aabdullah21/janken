c7b41e166d8a2429128a5d4448e7d4dc271032a8
Abdullah <abdullah056644@hotmail.com>
2022-09-13 22:02:22 +0300

    Finish game

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let playerWins = 0;
+let computerWins = 0;
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
     let computerChoice;
@@ -26,17 +28,14 @@ function getPlayerChoice() {
 }
 
 function isLose(playerSelection, computerSelection) {
-    rockLose = (playerSelection == 'rock' && computerSelection == 'paper');
-    paperLose = (playerSelection == 'paper' && computerSelection == 'scissors');
-    scissorsLose = (playerSelection == 'scissors' && computerSelection == 'rock');
+    let rockLose = (playerSelection == 'rock' && computerSelection == 'paper');
+    let paperLose = (playerSelection == 'paper' && computerSelection == 'scissors');
+    let scissorsLose = (playerSelection == 'scissors' && computerSelection == 'rock');
 
     return rockLose || paperLose || scissorsLose
 }
 
 function playRound(playerSelection, computerSelection) {
-
-    console.log(`Your choice is: ${playerSelection}`);
-    console.log(`Computer choice is: ${computerSelection}`);
 
 
 
@@ -45,50 +44,104 @@ function playRound(playerSelection, computerSelection) {
         result = 'draw';
 
     } else if (isLose(playerSelection, computerSelection)) {
+        computerWins++;
         result = 'computer'
 
     } else {
+        playerWins++;
         result = 'player'
     }
 
     return result;
 }
 
-function game() {
-    let playerSelection;
-    let computerSelection;
-    let playerWins = 0;
-    let computerWins = 0;
+const buttons = document.querySelectorAll('.choice');
+const container = document.querySelector('.container');
+const title = document.querySelector('.title')
+const btns = Array.from(buttons);
 
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i + 1}`);
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
-        let winner = playRound(playerSelection, computerSelection);
+for (const button of btns) {
+    button.addEventListener('click', (e) => {
 
-        if (winner == 'player') {
-            console.log(`Congratulations you won round ${i + 1}`);
-            playerWins++;
-        } else if (winner == 'computer') {
-            console.log(`Bad luck! Computer won round ${i + 1}`);
-            computerWins++;
-        } else {
-            console.log(`Round ${i + 1} is draw!`);
+        let playerSelection = e.target.classList.value;
+        let computerSelection = getComputerChoice();
+        let compImg = document.querySelector('.comp');
+        let playerImg = document.querySelector('.player');
+        compImg.setAttribute('src', `./imgs/${computerSelection}.svg`)
+        playerImg.setAttribute('src', `./imgs/${playerSelection}.svg`)
+        playRound(playerSelection, computerSelection);
+        updateResult();
+        if (gameOver()) {
+            title.classList.add('hidden');
+            container.classList.add('hidden')
+            winMessage();
         }
-
-    }
-
-    finalWinner(playerWins, computerWins);
-
-
+    })
 }
 
-function finalWinner(playerWins, computerWins) {
+function updateResult() {
+    let playerScore = document.querySelector('.player-score');
+    let compScore = document.querySelector('.computer-score');
+
+    playerScore.textContent = `Player score: ${playerWins}`;
+    compScore.textContent = `Computer score: ${computerWins}`;
+}
+
+function gameOver() {
+    return computerWins == 5 || playerWins == 5;
+}
+
+function winMessage() {
+    const announce = document.querySelector('.announce')
+    const finish = document.querySelector('.finish');
+    const refresh = document.querySelector('.refresh');
+
+
     if (computerWins > playerWins) {
-        console.log('The winner is computer!');
-    } else if (playerWins > computerWins) {
-        console.log('Congratulations you won!');
+
+        announce.textContent = 'Computer wins try again';
     } else {
-        console.log('It\'s draw!');
+        announce.textContent = 'Congratulations you win!';
     }
+    finish.classList.remove('hidden')
+    refresh.classList.remove('hidden');
+    refresh.addEventListener('click', () => window.location.reload());
 }
+// function game() {
+//     let playerSelection;
+//     let computerSelection;
+//     let playerWins = 0;
+//     let computerWins = 0;
+
+//     for (let i = 0; i < 5; i++) {
+//         console.log(`Round ${i + 1}`);
+//         playerSelection = getPlayerChoice();
+//         computerSelection = getComputerChoice();
+//         let winner = playRound(playerSelection, computerSelection);
+
+//         if (winner == 'player') {
+//             console.log(`Congratulations you won round ${i + 1}`);
+//             playerWins++;
+//         } else if (winner == 'computer') {
+//             console.log(`Bad luck! Computer won round ${i + 1}`);
+//             computerWins++;
+//         } else {
+//             console.log(`Round ${i + 1} is draw!`);
+//         }
+
+//     }
+
+//     finalWinner(playerWins, computerWins);
+
+
+// }
+
+// function finalWinner(playerWins, computerWins) {
+//     if (computerWins > playerWins) {
+//         console.log('The winner is computer!');
+//     } else if (playerWins > computerWins) {
+//         console.log('Congratulations you won!');
+//     } else {
+//         console.log('It\'s draw!');
+//     }
+// }
